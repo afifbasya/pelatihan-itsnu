7f92ad74343040ab9ce8421aec7dc2075ab43ad8
Afif Uddin <afifuddin@Afifs-MacBook-Air.local>
2024-06-15 09:16:25 +0700

    [UPDATE] API

--- pages/ambil_api.js
+++ pages/ambil_api.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export default function Home() {
     //looping
     const [users, setUsers] = useState([]); //array
+    const [tampil, setTampil] = useState(true);
 
     const ambilAPI = () => {
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -10,13 +11,29 @@ export default function Home() {
             .then(json => setUsers(json))
     }
 
+
+    //2. ketika ada state yang berubah
+    //3. ketika dicopot
+
+    //1. ketika pertama dimuncul
     useEffect(() => {
-        console.log("haloooooo");
+        // console.log("haloooooo");
         ambilAPI()
+    }, [])
+
+    useEffect(() => {
+        console.log("masukkk 2 ketika ada state yang berubah");
+    }, [tampil])
+
+    useEffect(() => {
+        console.log("masukkk 1");
     }, [])
 
     return (
         <div className="flex flex-col">
+
+            <button onClick={() => setTampil(!tampil)}>Ganti</button>
+
             {users.map((user) => {
                 return (
                     <div key={user.id}>
